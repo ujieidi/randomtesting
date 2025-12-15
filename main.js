c3fb33c9e1af6f6610c03792825432e1888da692
ujieidi <ujieidi@user.noreply.github.com>
2025-12-15 18:11:16 +0900

    start screen added v0.2

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ let defaultUser = {
   cost2: 100,
 };
 
+const menu = document.getElementById("menu");
+const mainGame = document.getElementById("mainGame");
+const playButton = document.getElementById("play");
 const button1 = document.getElementById("button1");
 const upgradeButton1 = document.getElementById("upgradeButton1");
 const upgradeButton2 = document.getElementById("upgradeButton2");
@@ -16,6 +19,12 @@ const pointText = document.getElementById("pointText");
 const upgradeText = document.getElementById("upgradeText");
 const upgradeText2 = document.getElementById("upgradeText2");
 const rngBG = document.getElementById("randomBackground");
+
+playButton.addEventListener("click", () => {
+  menu.style.display = "none";
+  mainGame.style.display = "block";
+  console.log("switched from menu to game screen")
+})
 
 function saveData() {
   localStorage.setItem("userData", JSON.stringify(user));
@@ -39,9 +48,16 @@ function updateText() {
 };
 
 resetButton.addEventListener("click", () => {
+  const userConfirmed = confirm("Are you sure to delete the data")
+  if (userConfirmed) {
   localStorage.removeItem("userData");
   user = { ...defaultUser };
   updateText()
+  console.log("resetted")
+  }
+  else {
+    console.log("cancelled")
+  }
 });
 
 button1.addEventListener("click", () => {
